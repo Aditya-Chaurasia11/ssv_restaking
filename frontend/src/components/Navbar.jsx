248f374b02a7ac200693cf7247b8178deba34355
Aditya Chaurasia <1212aditya11@gmail.com>
2024-08-30 18:46:21 +0530

    navbar changed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,20 +1,9 @@
 import React, { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 import "./navbar.css";
-import { useEffect } from "react";
-import { ethers } from "ethers";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { useTheme } from "./ThemeContext";
-import light from "../assets/light.svg";
-import dark from "../assets/dark.svg";
 
 const Navbar = () => {
-  const { theme, toggleTheme } = useTheme();
-
-  useEffect(() => {
-    document.body.className = theme === "light" ? "" : "dark";
-  }, [theme]);
-
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
@@ -27,47 +16,9 @@ const Navbar = () => {
       </div>
 
       <div className="gpt3__navbar-sign">
-        <button
-          type="button"
-
-          // onClick={loadAddress}
-        >
-          {/* {account
-            ? `${account?.slice(0, 6)}...${account?.slice(
-                account.length - 4,
-                account.length
-              )}`
-            : "Connect wallet"} */}
+        <button type="button">
           <ConnectButton />
         </button>
-        <button
-          onClick={toggleTheme}
-          style={{
-            padding: "10px",
-            backgroundColor: "transparent",
-            border: "none",
-            cursor: "pointer",
-            fontSize: "24px",
-          }}
-        >
-          {theme === "light" ? (
-            <img className="navbar_toogle" src={light}></img>
-          ) : (
-            <img className="navbar_toogle" src={dark}></img>
-          )}
-        </button>
-        {/* <ModeToggle/> */}
-        {/* <Link to="/collection">
-          <button type="button" className="navbar_my_nft_button">
-            Collection{" "}
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-              <path
-                fill="#ffffff"
-                d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z"
-              />
-            </svg>
-          </button>
-        </Link> */}
       </div>
     </div>
   );
